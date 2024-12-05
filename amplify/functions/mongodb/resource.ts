278053dbd15f732aaba75844f5dd9e38317e368d
muhammadnur-alim <muhammad.alim@magic-jungle.co.id>
2024-12-05 13:45:00 +0700

    change aws amplify

--- amplify/functions/mongodb/resource.ts
+++
@@ -1,8 +0,0 @@
-import { defineFunction } from "@aws-amplify/backend";
-
-export const connectMongoDb = defineFunction({
-  // optionally specify a name for the Function (defaults to directory name)
-  name: "connect to mongodb",
-  // optionally specify a path to your handler (defaults to "./handler.ts")
-  entry: "./handler.ts",
-});
